096cd8154d32fe9616bab4853d7d9a19d135365b
Febin-Joseph <febinachu123@gmail.com>
2023-05-15 12:47:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const fs = require('fs')
+
+let app = express()
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
+app.get('/api/v1/movies', (req, res) =>{
+    res.status(200).json({
+        status:"success",
+        count:movies.length,
+        data:{
+            movies:movies
+        }
+    })
+} )
+
+const port = 3000
+app.listen(port, () => {
+    console.log('server started on '+port)
+})
