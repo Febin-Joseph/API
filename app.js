d100ceaa2ac5874d70a60cfe07903e17362bffae
FEBIN JOSEPH <128711401+Febin-Joseph@users.noreply.github.com>
2023-05-15 14:29:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 let app = express()
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
 
+
+app.use(express.json())
+
 app.get('/api/v1/movies', (req, res) =>{
     res.status(200).json({
         status:"success",
@@ -13,6 +16,25 @@ app.get('/api/v1/movies', (req, res) =>{
         }
     })
 } )
+
+
+app.post('/api/v1/movies', (req, res) =>{
+    console.log(req.body)
+    const newId = movies[movies.length -1].id + 1
+    const newMovie = Object.assign({id :newId}, req.body)
+
+    movies.push(newMovie)
+    fs.writeFile('./data/movies.json', JSON.stringify(movies), (err) =>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                movies:newMovie
+            }
+        })
+    })
+} )
+
+
 
 const port = 3000
 app.listen(port, () => {
